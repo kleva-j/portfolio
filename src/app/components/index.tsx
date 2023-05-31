0f0698adb8f1e901aa5386a589e10956552805d3
kleva-j <kasmickleva@gmail.com>
2023-05-31 21:01:32 +0100

    feat: modify intro and add links to contact info

--- src/app/components/index.tsx
+++ src/app/components/index.tsx
@@ -1,10 +1,13 @@
 import { UseAppContext } from '@app/context';
+import email from '../../assets/at-sign.svg';
+import github from '../../assets/github.svg';
+import linkedin from '../../assets/linkedin.svg';
 
 export const Index = () => {
 	const { aboutMe, profileImg } = UseAppContext();
 
 	return (
-		<section className="flex w-full max-w-3xl mx-auto mt-24 px-4 justify-between">
+		<section className="flex w-full max-w-3xl mx-auto mt-24 lg:mt-[160px] px-4 justify-between">
 			<article className="w-full md:w-6/12 flex flex-col gap-y-8">
 				<div>
 					<h1 className="text-[46px] font-bold leading-10 text-accent uppercase">{aboutMe.name}</h1>
@@ -16,16 +19,31 @@ export const Index = () => {
 
 				<div className="flex flex-col gap-5">
 					<span className="">
-						{`I'm an experienced software engineer who constantly seeks out innovative solutions to everyday problems. In
-						my four years in this industry, I've honed my analytical thinking and collaboration skills, and I love
-						working with a team. I've also had the opportunity to serve as the software engineer lead for three projects
-						with First Technology`}
+						{`I'm an experienced software developer with a passion for building robust and scalable applications. Proficient in
+						multiple programming languages and frameworks, with a keen eye for detail and a drive for continuous
+						learning.`}
 					</span>
 
 					<span>
-						I build software solutions using <strong>JavaScript</strong>, <strong>React</strong> and{' '}
+						I build software solutions using <strong>TypeScript</strong>, <strong>React</strong> and{' '}
 						<strong>NodeJS</strong>
 					</span>
+				</div>
+				<div className="border-t border-[#006DAA] border-dotted dashed pt-3 -mt-3 flex gap-x-4">
+					<a
+						href="https://www.linkedin.com/in/michael-obasi-808806140/"
+						title="linkedin"
+						target="_blank"
+						rel="noreferrer noopener"
+					>
+						<img className="text-[#006DAA]" src={linkedin} alt="linkedin icon" />
+					</a>
+					<a href="https://github.com/kleva-j" title="github" target="_blank" rel="noreferrer noopener">
+						<img className="text-[#006DAA]" src={github} alt="github icon" />
+					</a>
+					<a href="mailto:kasmickleva@gmail.com" title="email" target="_blank" rel="noreferrer noopener">
+						<img className="text-[#006DAA]" src={email} alt="at sign icon" />
+					</a>
 				</div>
 			</article>
 			<article className="hidden md:block w-4/12">
